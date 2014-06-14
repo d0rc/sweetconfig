e3549c206e055ecfd72d4cf6d0cadaf200df7e36
d0rc <7993863@gmail.com>
2014-06-14 23:28:40 +0400

    add conversion

--- lib/sweetconfig.ex
+++ lib/sweetconfig.ex
@@ -39,11 +39,34 @@ defmodule Sweetconfig.Utils do
     end
   end
 
+  defp pre_process(int) when is_integer(int), do: int
+  defp pre_process(atom) when is_atom(atom), do: atom
+  defp pre_process(bin) when is_binary(bin), do: bin
+  defp pre_process(list) when is_list(list) do
+    for el <- list, into: [] do
+      pre_process(el)
+    end
+  end
+  defp pre_process(%{type: type, value: value}) when is_binary(value) do
+    case type do
+      :list -> :erlang.binary_to_list(value)
+    end
+  end
+  defp pre_process(map) when is_map(map) do
+    for {k, v} <- map, into: %{} do
+      {k, pre_process(v)}
+    end
+  end
+  defp pre_process({k,v}) do 
+    pre_process({k, pre_process(v)})
+  end
+
   defp push_to_ets([]), do: []
   defp push_to_ets([configs]) do
     for {key, value} <- configs do
-      :ets.insert(:sweetconfig, {key, value})
+      :ets.insert(:sweetconfig, {key, pre_process(value)})
     end
+    configs
   end
   defp push_to_ets(configs) when is_list(configs) do
     case Enum.all?(configs, &is_map/1) do
